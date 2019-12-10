cca508b0cf50e0c9e224e74e9ece9ebe4b1dc6e4
Artemy <artemy.gordon@mail.ru>
2019-12-11 00:15:55 +0300

    Made more beautiful display of days months and months of the year

--- src/main/java/com/jaz7/reminder/DateTimeParser.java
+++ src/main/java/com/jaz7/reminder/DateTimeParser.java
@@ -5,13 +5,15 @@ import com.jaz7.inputOutput.UserIO;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Calendar;
+import java.time.Month;
+import java.util.*;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
 
 public class DateTimeParser {
     public static String[] years;
     public static LocalDateTime currentDate;
-    private static String[] months;
+    private static String[] currentYearMonths;
     private static String[] days;
     private static UserIO userIO;
     private static final Logger LOGGER = Logger.getLogger(DateTimeParser.class.getSimpleName());
@@ -27,9 +29,17 @@ public class DateTimeParser {
                 Integer.toString(currentDate.getYear()),
                 Integer.toString(currentDate.plusYears(1).getYear()),
                 "Other"};
-        months = new String[12];
-        for (int i = 0; i < 12; i++) {
-            months[i] = currentDate.plusMonths(i).getMonth().toString();
+        if (currentDate.getMonth().getValue() == 1)
+            currentYearMonths = Arrays.stream(Month.values()).map(Enum::toString).toArray(String[]::new);
+        else {
+            List<String> yearMonths = new ArrayList<>();
+            for (int i = 0; i < 12; i++) {
+                Month month = currentDate.plusMonths(i).getMonth();
+                if (month.getValue() != 1)
+                    yearMonths.add(month.toString());
+                else break;
+            }
+            currentYearMonths = yearMonths.toArray(new String[0]);
         }
         LOGGER.info("Current date has been updated");
     }
@@ -37,8 +47,12 @@ public class DateTimeParser {
     public static AddingState setYear(Calendar rawDate, int year, String chatId, AddingState addingState) {
         rawDate.set(Calendar.YEAR, year);
         LOGGER.info(String.format("%s: Year has been set", chatId));
-
-        userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseMonth"), months, chatId);
+        if (year == currentDate.getYear())
+            userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseMonth"), currentYearMonths, chatId);
+        else {
+            String[] yearMonths = Arrays.stream(Month.values()).map(Enum::toString).toArray(String[]::new);
+            userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseMonth"), yearMonths, chatId);
+        }
         if (addingState == AddingState.SET_YEAR) {
             return AddingState.SET_MONTH;
         } else {
@@ -47,21 +61,35 @@ public class DateTimeParser {
     }
 
     public static AddingState setMonth(Calendar rawDate, int month, String chatId, AddingState addingState) {
-        rawDate.set(Calendar.MONTH, currentDate.plusMonths(month).getMonthValue());
+        rawDate.set(Calendar.MONTH, month - 1);
         LOGGER.info(String.format("%s: Month has been set", chatId));
-
         int daysInMonth = getDaysInMonth(rawDate);
         days = new String[daysInMonth];
-        for (int i = 1; i <= daysInMonth; i++) {
-            days[i - 1] = Integer.toString(i);
+        for (int i = 0; i < daysInMonth; i++) {
+            days[i] = Integer.toString(i + 1);
         }
         Reminder.users.get(chatId).noteKeeper.noteAdder.daysInCurrentMonth = days;
-        userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseDay"), days, chatId);
+        //todo разобраться зачем здесь это присовение, какое дальнейший смысл оно имеет
+        String[] daysForMonth = getDaysForMonth(rawDate, daysInMonth);
+        userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseDay"), daysForMonth, chatId);
         if (addingState == AddingState.SET_MONTH) {
             return AddingState.SET_DAY;
         } else {
             return AddingState.SET_REMIND_DAY;
         }
+    }
+
+    private static String[] getDaysForMonth(Calendar rawDate, int daysInMonth) {
+        List<String> days = new ArrayList<>();
+        if (currentDate.getMonthValue() - 1 != rawDate.get(Calendar.MONTH) ||
+                currentDate.getYear() != rawDate.get(Calendar.YEAR))
+            rawDate.set(Calendar.DAY_OF_MONTH, 1);
+        int startDayOfWeek = rawDate.get(Calendar.DAY_OF_WEEK) == 1 ? 7 : rawDate.get(Calendar.DAY_OF_WEEK) - 1;
+        for (int i = 1; i < startDayOfWeek; i++)
+            days.add(" ");
+        for (int i = rawDate.get(Calendar.DAY_OF_MONTH); i <= daysInMonth; i++)
+            days.add(Integer.toString(i));
+        return days.toArray(String[]::new);
     }
 
     public static AddingState setDay(Calendar rawDate, int day, String chatId, AddingState addingState) {
@@ -78,13 +106,13 @@ public class DateTimeParser {
     public static AddingState setTime(Calendar rawDate, LocalTime time, String chatId, AddingState addingState) {
         rawDate.set(Calendar.HOUR_OF_DAY, time.getHour());
         rawDate.set(Calendar.MINUTE, time.getMinute());
-
         // Если время в прошлом, то заново задаем всю дату, начиная с месяца
         if (LocalDateTime.ofInstant( // TODO Исправить - Сделать так, чтобы новая дата задавалась с шага, на котором была сделана ошибка. Т.е. Добавить на каждом шаге проверку
                 rawDate.toInstant(),
                 rawDate.getTimeZone().toZoneId()).isBefore(LocalDateTime.now())) {
             userIO.showMessage(BotOptions.botAnswers.get("WrongDate"), chatId);
-            userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseMonth"), months, chatId);
+            userIO.showOnClickButton(BotOptions.botAnswers.get("ChooseMonth"), currentYearMonths, chatId);
+            // todo Разобраться с currentYearMonths, какое оно тут должно быть
             LOGGER.info(String.format("%s: Wrong time (in the past)", chatId));
             if (addingState == AddingState.SET_TIME) {
                 return AddingState.SET_MONTH;
