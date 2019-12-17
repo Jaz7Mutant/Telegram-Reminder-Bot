4c59aa46dba391049ce915fa9c4eaa5b5e7052e1
JazzMutant <d-a-ny4@ya.ru>
2019-12-17 21:17:40 +0500

    Refactored and fixed /next function

--- src/main/java/com/jaz7/chatRoulette/ChatRoulette.java
+++ src/main/java/com/jaz7/chatRoulette/ChatRoulette.java
@@ -26,15 +26,19 @@ public class ChatRoulette {
     public void startChatting(String command, String chatId) {
         LOGGER.info(String.format("%s: Switch to chatting state", chatId));
         userIO.showMessage(BotOptions.botAnswers.get("CurrentOnline") + readyToChatUsers.size(), chatId);
-        chattingUsers.add(chatId);
-        readyToChatUsers.add(chatId);
-        switchCompanion(null, chatId);
+        if (!chattingUsers.contains(chatId)) {
+            chattingUsers.add(chatId);
+            readyToChatUsers.add(chatId);
+            switchCompanion(null, chatId);
+        }
     }
 
     public void switchCompanion(String command, String chatId) {
         LOGGER.info(String.format("%s: Finding new companion", chatId));
-        if (!readyToChatUsers.contains(chatId)) {
-            readyToChatUsers.add(chatId);
+        if (!chattingUsers.contains(chatId)) {
+            LOGGER.info(String.format("%s: User is not in chatting state", chatId));
+            userIO.showMessage(BotOptions.botAnswers.get("JoinChatFirst"), chatId);
+            return;
         }
         String currentCompanion = users.get(chatId).companionChatId;
         if (currentCompanion != null) {
@@ -45,36 +49,35 @@ public class ChatRoulette {
             }
             if (users.get(currentCompanion).companionChatId != null) {
                 userIO.showMessage(BotOptions.botAnswers.get("CompanionLeft"), currentCompanion);
-                userIO.showMessage(BotOptions.botAnswers.get("LookingForCompanion"), currentCompanion);
             }
             users.get(currentCompanion).companionChatId = null;
+            users.get(chatId).companionChatId = null;
             readyToChatUsers.add(currentCompanion);
+            readyToChatUsers.add(chatId);
+            switchCompanion(null, currentCompanion);
         }
-        users.get(chatId).companionChatId = null;
         userIO.showMessage(BotOptions.botAnswers.get("LookingForCompanion"), chatId);
-        if (readyToChatUsers.size() == 1) {
+        String companion = getRandomUserToChat(chatId);
+        if (companion.equals("")) {
             userIO.showMessage(BotOptions.botAnswers.get("NoOnlineUsers"), chatId);
             LOGGER.info(String.format("%s: No free users to chat", chatId));
-        } else {
-            String companion = getRandomUserToChat(chatId);
-            if (companion.equals("")) {
-                userIO.showMessage(BotOptions.botAnswers.get("NoOnlineUsers"), chatId);
-                LOGGER.info(String.format("%s: No free users to chat", chatId));
-                return;
-            }
-            users.get(chatId).companionChatId = companion;
-            users.get(companion).companionChatId = chatId;
-            userIO.showMessage(BotOptions.botAnswers.get("ChattingWith") + chatId, companion);
-            userIO.showMessage(BotOptions.botAnswers.get("ChattingWith") + companion, chatId);
-            readyToChatUsers.remove(chatId);
-            readyToChatUsers.remove(companion);
-            LOGGER.info(String.format("%s: Connection completed with %s", chatId, companion));
-            LOGGER.info(String.format("%s: Connection completed %s", companion, chatId));
+            return;
         }
+        users.get(chatId).companionChatId = companion;
+        users.get(companion).companionChatId = chatId;
+        userIO.showMessage(BotOptions.botAnswers.get("ChattingWith") + chatId, companion);
+        userIO.showMessage(BotOptions.botAnswers.get("ChattingWith") + companion, chatId);
+        readyToChatUsers.remove(chatId);
+        readyToChatUsers.remove(companion);
+        LOGGER.info(String.format("%s: Connection completed with %s", chatId, companion));
+        LOGGER.info(String.format("%s: Connection completed %s", companion, chatId));
     }
 
     public void stopChatting(String command, String chatId) {
         LOGGER.info(String.format("%s: Switching off chatting", chatId));
+        if (!chattingUsers.contains(chatId)){
+            return;
+        }
         chattingUsers.remove(chatId);
         readyToChatUsers.remove(chatId);
         String currentCompanion = users.get(chatId).companionChatId;
@@ -89,9 +92,9 @@ public class ChatRoulette {
     }
 
     private String getRandomUserToChat(String finderChatId) {
-        String user = "";
+        String user;
         int i = 0;
-        while (user.equals("") || user.equals(finderChatId) || users.get(finderChatId).bannedUsers.contains(user)) {
+        do {
             LOGGER.info(String.format("%s: Trying to find companion...", finderChatId));
             if (i >= readyToChatUsers.size()) {
                 return "";
@@ -99,6 +102,8 @@ public class ChatRoulette {
             user = readyToChatUsers.get(i);
             i++;
         }
+        while (user.equals(finderChatId) || users.get(finderChatId).bannedUsers.contains(user)
+                || users.get(user).bannedUsers.contains(finderChatId));
         return user;
     }
 }
--- src/main/java/com/jaz7/event/EventStatus.java
+++
@@ -1,7 +0,0 @@
-package com.jaz7.event;
-
-public enum EventStatus {
-    COMING_SOON,
-    AVAILABLE,
-    UNAVAILABLE
-}
